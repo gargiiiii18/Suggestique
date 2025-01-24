5b8f7e2cab24ee6562780fa6c44778290ed365d4
Gargi Sukhatankar <gargi.p.sukhatankar@gmail.com>
2025-01-24 17:33:40 +0530

    added api products route

---
+++ src/app/api/products/route.js
@@ -0,0 +1,33 @@
+import {initMongoose} from "../../../lib/mongoose";
+import Product from "../../../models/Product";
+
+ export async function GET(req, res) {
+    await initMongoose();
+    const { searchParams } = req.nextUrl;
+    const ids = searchParams.get('ids');
+    // console.log(ids);
+    
+    if(ids){
+      const idArray = ids.split(',');
+      const productsInCart = await Product.find({'_id' : {$in: idArray}}).exec();
+      // console.log(JSON.stringify(productsInCart));
+      
+      return new Response(JSON.stringify(productsInCart), {
+        headers: {"Content-Type": "application/json"},
+      });
+    } else{
+      if(ids == ''){
+        // console.log(JSON.stringify({ message: "Your cart is empty" }));
+        
+        return new Response(JSON.stringify({ message: "Your cart is empty" }), {
+          headers: { "Content-Type": "application/json" },
+          // status: 404,
+        });
+      } else{
+      const products = await Product.find().exec();
+      return new Response(JSON.stringify(products), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+    }
+  }
