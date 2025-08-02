27068e17af5ac99b373a45f61abc00d4f1239f0c
gargi <gargi.p.sukhatankar@gmail.com>
2025-08-03 01:52:37 +0530

    wrapped session provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import {ProductsContextProvider} from "./contexts/ProductsContext";
 import "./globals.css";
+import AuthProvider from "./contexts/AuthProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <AuthProvider>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -32,6 +34,7 @@ export default function RootLayout({
         {children}
         </ProductsContextProvider>
       </body>
+      </AuthProvider>
     </html>
   );
 }
